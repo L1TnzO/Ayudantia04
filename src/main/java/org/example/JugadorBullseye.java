bd8923c064aa68c838e497abb4248ff70087236a
L1TnzO <thenzo22@gmail.com>
2023-06-17 20:25:09 -0400

    Cambios menores

--- src/main/java/org/example/JugadorBullseye.java
+++ src/main/java/org/example/JugadorBullseye.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class JugadorBullseye extends Jugador {
-    private int dinero;
+    private Integer dinero;
     private Caballo caballo;
 
     public JugadorBullseye(String nombre, int dinero, Caballo caballo) {
@@ -9,12 +9,15 @@ public class JugadorBullseye extends Jugador {
         this.dinero = dinero;
         this.caballo = caballo;
     }
+
     public Caballo getCaballo() {
         return caballo;
     }
+
     public void setCaballo(Caballo caballo) {
         this.caballo = caballo;
     }
+
     public int getDinero() {
         return dinero;
     }
@@ -23,3 +26,4 @@ public class JugadorBullseye extends Jugador {
         this.dinero = dinero;
     }
 }
+
